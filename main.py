0808b9e5069541f74bf8d1219b661604c79a1555
Mixtli Monroe <nikolai.mixtli@gmail.com>
2025-04-13 16:18:31 +0100

    Action minimising using Bezier curves

--- main.py
+++ main.py
@@ -125,8 +125,9 @@ if __name__ == "__main__":
     initial_pos = np.array([0, 0])
     final_pos = np.array([3, -1])
     degree = 4
-    initial_guess = [[.5, -.5], [.5, -.5], [.5, -.5]]
+    initial_guess = [[.5, -.5] for _ in range(degree-1)]
 
+    # Create an instance of ActionMinimiser and minimize the action for the Brachistochrone problem
     Brachistochrone = ActionMinimiser(Lagrangian, degree, initial_pos, final_pos)
     Brachistochrone.minimise(initial_guess)
     Brachistochrone.plot_bezier_curve()
