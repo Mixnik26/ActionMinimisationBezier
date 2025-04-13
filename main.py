7b5bc86e39a192155805bd1905930e1bd2bd23cf
Mixtli Monroe <nikolai.mixtli@gmail.com>
2025-04-13 16:23:29 +0100

    Action minimising using Bezier curves

--- main.py
+++ main.py
@@ -97,22 +97,3 @@ class ActionMinimiser:
         plt.legend()
         plt.grid()
         plt.show()
-
-if __name__ == "__main__":
-    # Example Lagrangian
-    # Lagrangian for the Brachistochrone problem
-    def Lagrangian(t, q, qdot):
-        x, y = q
-        xdot, ydot = qdot
-        return np.sqrt((xdot**2 + ydot**2)/(-2*y))
-
-    # Initial conditions
-    initial_pos = np.array([0, 0])
-    final_pos = np.array([3, -1])
-    degree = 4
-    initial_guess = [[.5, -.5] for _ in range(degree-1)]
-
-    # Create an instance of ActionMinimiser and minimize the action for the Brachistochrone problem
-    Brachistochrone = ActionMinimiser(Lagrangian, degree, initial_pos, final_pos)
-    Brachistochrone.minimise(initial_guess)
-    Brachistochrone.plot_bezier_curve()
